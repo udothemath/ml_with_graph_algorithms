aad0cc786a7a698f47922001a1b863f115c78661
udothemath <fuchang.sun@gmail.com>
2021-12-20 10:28:41 +0800

    docs: edit readme

---
+++ ex_colab2/README.md
@@ -0,0 +1,6 @@
+## colab連結
+- [colab2](https://colab.research.google.com/drive/1Aa0eKSmyYef1gORvlHv7EeQzSVRb30eL?usp=sharing)
+
+## Note
+- How to read and make tensor matrix using SparseTensor?
+
--- ex_colab2/run_colab2.py
+++ ex_colab2/run_colab2.py
@@ -127,12 +127,9 @@ dataset = PygNodePropPredDataset(name=dataset_name,
                                  root=DIR_DATA_OGB,
                                  transform=T.ToSparseTensor())
 data = dataset[0]
+print(data)
 
 # %%
-# print (data.adj_t)
-
-# %%
-
 # Make the adjacency matrix to symmetric
 data.adj_t = data.adj_t.to_symmetric()
 
@@ -144,4 +141,76 @@ print('Device: {}'.format(device))
 data = data.to(device)
 split_idx = dataset.get_idx_split()
 train_idx = split_idx['train'].to(device)
+
 # %%
+# print (split_idx.keys())
+for the_key in split_idx.keys():
+    check_item = split_idx[the_key] 
+    print(f"{the_key}(size: {len(check_item)}): {check_item[:5]}")
+
+# %%
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers,
+                 dropout, return_embeds=False):
+        # TODO: Implement this function that initializes self.convs, 
+        # self.bns, and self.softmax.
+
+        super(GCN, self).__init__()
+
+        # A list of GCNConv layers
+        self.convs = None
+
+        # A list of 1D batch normalization layers
+        self.bns = None
+
+        # The log softmax layer
+        self.softmax = None
+
+        ############# Your code here ############
+        ## Note:
+        ## 1. You should use torch.nn.ModuleList for self.convs and self.bns
+        ## 2. self.convs has num_layers GCNConv layers
+        ## 3. self.bns has num_layers - 1 BatchNorm1d layers
+        ## 4. You should use torch.nn.LogSoftmax for self.softmax
+        ## 5. The parameters you can set for GCNConv include 'in_channels' and 
+        ## 'out_channels'. More information please refer to the documentation:
+        ## https://pytorch-geometric.readthedocs.io/en/latest/modules/nn.html#torch_geometric.nn.conv.GCNConv
+        ## 6. The only parameter you need to set for BatchNorm1d is 'num_features'
+        ## More information please refer to the documentation: 
+        ## https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm1d.html
+        ## (~10 lines of code)
+
+        #########################################
+
+        # Probability of an element to be zeroed
+        self.dropout = dropout
+
+        # Skip classification layer and return node embeddings
+        self.return_embeds = return_embeds
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, adj_t):
+        # TODO: Implement this function that takes the feature tensor x,
+        # edge_index tensor adj_t and returns the output tensor as
+        # shown in the figure.
+
+        out = None
+
+        ############# Your code here ############
+        ## Note:
+        ## 1. Construct the network as showing in the figure
+        ## 2. torch.nn.functional.relu and torch.nn.functional.dropout are useful
+        ## More information please refer to the documentation:
+        ## https://pytorch.org/docs/stable/nn.functional.html
+        ## 3. Don't forget to set F.dropout training to self.training
+        ## 4. If return_embeds is True, then skip the last softmax layer
+        ## (~7 lines of code)
+
+        #########################################
+
+        return out
