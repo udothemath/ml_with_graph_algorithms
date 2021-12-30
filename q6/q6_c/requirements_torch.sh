a085ccf599239a5f1467e7163e0f028149cd6e91
udothemath <fuchang.sun@gmail.com>
2021-12-30 13:21:10 +0800

    features: add results with gpu computation

--- q6/q6_c/requirements_torch.sh
+++ q6/q6_c/requirements_torch.sh
@@ -5,3 +5,6 @@ pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${C
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-geometric
+
+pip install --upgrade pip
+pip install ipykernel
