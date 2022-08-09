9ac1e8ae00ae64bb5f0b815c234f56712cd1f87a
jiangjiawei1103 <waynechuang97@gmail.com>
2022-08-09 17:14:51 +0800

    feat: Enable recording of profiling result for benchmarking

--- etl_study/etl_study/tools/profile_main.py
+++ etl_study/etl_study/tools/profile_main.py
@@ -4,6 +4,8 @@ import os
 from argparse import Namespace
 from datetime import datetime
 
+import pandas as pd
+
 from engine.defaults import BaseArgParser
 from profile_framework.etl_profiler import ETLProfiler, ETLProfileResult
 from utils.logger import Logger
@@ -42,6 +44,12 @@ class ETLProfileArgParser(BaseArgParser):
             default=None,
             help="number of profilings to run",
         )
+        self.argparser.add_argument(
+            "--to-benchmark",
+            type=self._str2bool,
+            default=None,
+            help="whether to append profiling result for further benchmarking",
+        )
 
 
 def _setup_logging_path(query: str) -> str:
@@ -74,7 +82,7 @@ def _setup_logging_path(query: str) -> str:
     return logging_path
 
 
-def _dump(etl_profile_result: ETLProfileResult, logging_path: str) -> None:
+def _dump_profile_result(etl_profile_result: ETLProfileResult, logging_path: str) -> None:
     """Dump ETL operation profiling result.
 
     Parameters:
@@ -86,6 +94,40 @@ def _dump(etl_profile_result: ETLProfileResult, logging_path: str) -> None:
     """
     with open(os.path.join(logging_path, "prf.json"), "w") as f:
         json.dump(etl_profile_result._asdict(), f)
+
+
+def _add_profile_result_to_berk(args: Namespace, etl_profile_result: ETLProfileResult) -> None:
+    """Append profiling result for further benchmarking.
+
+    Parameters:
+        args: arguments driving ETL operation profiling process
+        etl_profile_result: ETL operation profiling result
+
+    Return:
+        None
+    """
+    flatten = lambda x: {
+        f"{prf_ind_name}-{stats_name}": prf_val
+        for prf_ind_name, prf_summary in x._asdict().items()
+        for stats_name, prf_val in prf_summary.items()
+    }
+
+    etl_profile_result = flatten(etl_profile_result)
+    etl_profile_result = {
+        "query": args.query,
+        "input_file": args.input_file,
+        "mode": args.mode,
+        "n_profiles": args.n_profiles,
+        **etl_profile_result,
+    }
+    berk_new_row = pd.DataFrame(etl_profile_result, index=[0])
+    if os.path.exists("./berk.csv"):  # Tmp fixed
+        berk = pd.read_csv("./berk.csv")
+        berk = pd.concat([berk, berk_new_row], ignore_index=True)
+    else:
+        berk = berk_new_row
+
+    berk.to_csv("./berk.csv", index=False)
 
 
 def main(args: Namespace) -> None:
@@ -114,7 +156,11 @@ def main(args: Namespace) -> None:
     etl_profiler.run()
 
     # Dump profiling result
-    _dump(etl_profiler.etl_profile_result_, logging_path)
+    _dump_profile_result(etl_profiler.etl_profile_result_, logging_path)
+
+    # Append profiling result for further benchmarking
+    if args.to_benchmark:
+        _add_profile_result_to_berk(args, etl_profiler.etl_profile_result_)
 
 
 if __name__ == "__main__":
